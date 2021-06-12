ca00034d3f8ccfcd8a22a8285f8f46cb99b25865
tildeman <luucongthehien@gmail.com>
2021-06-12 17:32:05 +0700

    Added login stuff

--- index.php
+++ index.php
@@ -2,6 +2,28 @@
 require("config.php");
 session_start();
 $conn=new mysqli($config["db_server"],$config["db_uname"],$config["db_pass"],$config["db_name"]);
+if (isset($_POST["username"])&&isset($_POST["password"])){
+	$uname_query=$conn->query("SELECT * FROM ten_nguoi_dung WHERE ten='".$conn->real_escape_string($_POST["username"])."'");
+	if ($uname_query->num_rows==0){
+		$error="Invalid username";
+	}
+	else{
+		$row=$uname_query->fetch_array();
+		$hash=$row["pass"];
+		if ($hash==sha1(sha1($_POST["password"]))){
+			if (!in_array("disabled",explode(" ",$row["properties"]))){
+				$_SESSION["username"]=$_POST["username"];
+				$_SESSION["password"]=$_POST["password"];
+			}
+			else{
+				$error="Account is disabled";
+			}
+		}
+		else{
+			$error="Invalid password";
+		}
+	}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,32 +32,60 @@ $conn=new mysqli($config["db_server"],$config["db_uname"],$config["db_pass"],$co
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/main.css">
-	<title>XMake - Trang chủ</title>
+	<title>XMake</title>
 </head>
 <body>
+	<script>
+		console.log("Powered by XMake :)");
+	</script>
 	<div class="main-bg">
-		<div id="menubar">
-			<a href=".">
-				<img src="images/xmake-logo.png" id="home-btn">
-			</a>
-			<div class="menu-btn">
-				<a href="?xmakereq=baiviet">Bài viết</a>
-			</div>
-			<div class="menu-btn">
-				<a href="?xmakereq=giaodien">Giao diện</a>
-			</div>
-			<div class="menu-btn">
-				<a href="?xmakereq=tienich">Tiện ích</a>
-			</div>
-			<div class="menu-btn">
-				<a href="?xmakereq=logout">Đăng xuất</a>
-			</div>
-		</div>
-		<div id="body">
-			<?php
-				include $config["docroot"]."/xuli.php";
+		<?php
+		if (isset($error)){
 			?>
-		</div>
+			<div class="notification_error notification"><span><?=$error?></span></div>
+			<?php
+		}
+		if (isset($_SESSION["username"])&&isset($_SESSION["password"])){
+			?>
+			<div id="menubar">
+				<a href=".">
+					<img src="images/xmake-logo.png" id="home-btn">
+				</a>
+				<div class="menu-btn">
+					<a href="?xmakereq=baiviet">Bài viết</a>
+				</div>
+				<div class="menu-btn">
+					<a href="?xmakereq=giaodien">Giao diện</a>
+				</div>
+				<div class="menu-btn">
+					<a href="?xmakereq=tienich">Tiện ích</a>
+				</div>
+				<div class="menu-btn">
+					<a href="?xmakereq=logout">Đăng xuất</a>
+				</div>
+			</div>
+			<div id="body">
+				<?php
+					include $config["docroot"]."/xuli.php";
+				?>
+			</div>
+			<?php
+		}
+		else{
+			?>
+			<div class="login-box">
+				<form method="post">
+					<h1>Đăng nhập vào XMake</h1>
+					<span>Tên người dùng:</span><br>
+					<input type="text" name="username" class="login-expand">
+					<span>Mật khẩu:</span><br>
+					<input type="password" name="password" class="login-expand">
+					<input type="submit" name="login-submit" value="Đăng nhập" id="login-submit">
+				</form>
+			</div>
+			<?php
+		}
+		?>
 	</div>
 </body>
 </html>
