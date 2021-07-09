94cbd50096a8123f904bf59db7662cbc11801ef7
tildeman <luucongthehien@gmail.com>
2021-07-09 17:03:35 +0700

    little design mod

--- wrappers/giaodien.php
+++ wrappers/giaodien.php
@@ -10,24 +10,26 @@ if (isset($_GET["muc"])){
 }
 else{
 	?>
-	<div class="gditem">
-		<img src="images/sec_icn/thanhdoc.svg" onclick="location='?xmakereq=giaodien&muc=thanhdoc'">Thanh dọc
-	</div>
-	<div class="gditem" onclick="console.log('thanh ngang')">
-		<img src="images/sec_icn/thanhngang.svg" onclick="location='?xmakereq=giaodien&muc=thanhngang'">Thanh ngang
-	</div>
-	<div class="gditem">
-		<img src="images/sec_icn/ttinchung.svg" onclick="location='?xmakereq=giaodien&muc=ttinchung'">Thông tin chung
-	</div>
-	<div class="gditem">
-		<img src="images/sec_icn/trangchu.svg" onclick="location='?xmakereq=giaodien&muc=trangchu'">Trang chủ
-	</div>
-	<script>
-	let b=document.getElementsByClassName("gditem");
-	for (let i=0;i<b.length;i++){
-		b[i].style.cursor="pointer";
-	}
-	</script>
+	<a href="?xmakereq=giaodien&muc=thanhdoc">
+		<div class="gditem">
+			<img src="images/sec_icn/thanhdoc.svg">Thanh dọc
+		</div>
+	</a>
+	<a href="?xmakereq=giaodien&muc=thanhngang">
+		<div class="gditem" onclick="console.log('thanh ngang')">
+			<img src="images/sec_icn/thanhngang.svg">Thanh ngang
+		</div>
+	</a>
+	<a href="?xmakereq=giaodien&muc=ttinchung">
+		<div class="gditem">
+			<img src="images/sec_icn/ttinchung.svg">Thông tin chung
+		</div>
+	</a>
+	<a href="?xmakereq=giaodien&muc=trangchu">
+		<div class="gditem">
+			<img src="images/sec_icn/trangchu.svg">Trang chủ
+		</div>
+	</a>
 	<?php
 }
 ?>
